a8d1413b7e2cdb5627066c2ecd6f6040694ac410
Ferris Boran <ferris.boran@gmail.com>
2019-03-11 22:14:14 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,12 +13,18 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  x = ""
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
+
+=begin
+  x = ""
   cheese_types.any? do |cheese|
   	if array.include?(cheese)
   		x << cheese
   	end
   end
   x == "" ? nil : x
+=end
 end
