9d95438a0d60f645385ab194cb29f7582274e258
virginia koar <virginiakoar@TimKoar.fios-router.home>
2019-04-07 18:51:03 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,25 +1,26 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf, index|
-    puts "#{index + 1}. #{dwarf}"
+  dwarves.each_with_index do |dwarf, i|
+    puts "#{i+1}. #{dwarf}"
   end
 end
 
+
 def summon_captain_planet(array)
-  array.collect do |element|
-  element.capitalize!
-  element << "!"
+  array.map do |item|
+  item.capitalize << "!"
   end
 end
 
 
 def long_planeteer_calls(calls)
-  calls.any? do |call| call.length > 4
+  calls.any? do |call|
+    call.length > 4
   end
 end
 
 def find_the_cheese(cheeses)
-
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheeses.find do |cheese| cheese_types.include?(cheese)
+  cheeses.find do |cheese|
+    cheese_types.include?(cheese)
   end
 end
